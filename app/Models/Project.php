de7d95a1798cf5206591d3436279afec8638dbf6
MaTtiaa01 <mattia2001@live.it>
2023-01-17 15:48:51 +0100

    try to add pivot table migration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Technology;
+use App\Models\Type;
 
 class Project extends Model
 {
@@ -14,5 +17,15 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->BelongsTo(Type::class);
+    }
+
+    /**
+     * The tags that belong to the Technology
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
